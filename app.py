01bd5aedb259dd2a2cd4aea96b2bbdb6388aaf00
nimashakonara <119902763+nimashakonara@users.noreply.github.com>
2025-06-30 16:34:07 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+print('This is my first program')
+
+a=7
+b=5.5
+print(a)
+print(b)
+# Write your code for Exercise 2 in this cell
+myint=200
+print(myint)
+# a,b and c are variables and addition operation is tested in this program 
+a=25 
+b=100 
+c=a+b
+print (c) 
+# Write your code for Exercise 3 in this cell
+a=20
+c=a*100
+print(c)
+
+score=88
+if score>90:
+    print("Well done! That gets you an A+ grade")
+else:
+    print("Better luck next time!")
+# Write your code for Exercise 5 in this cell
+myval=8
+if myval>0:
+        print("It is a positive number")
+else:
+        print("It is is a negative number")
+        # Declare and Print an Array Using a For-Loop
+mylist = [1, 2, 3, 4, 5]
+for i in mylist:
+    print(i)    
